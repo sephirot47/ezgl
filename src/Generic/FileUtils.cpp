f15dbb6d7e516cc630a5ad25d1cc015b79b0519a
sephirot47 <victorantondominguez@gmail.com>
2020-07-21 20:13:09 +0200

    Added exception to GetFileContents

--- src/Generic/FileUtils.cpp
+++ src/Generic/FileUtils.cpp
@@ -1,10 +1,17 @@
 #include "ez/FileUtils.h"
+#include "ez/Macros.h"
 #include <fstream>
 
 namespace ez
 {
 std::string GetFileContents(const std::filesystem::path& filepath)
 {
+  if (!std::filesystem::exists(filepath))
+    THROW_EXCEPTION("File " << filepath << " does not exist");
+
+  if (!std::filesystem::is_regular_file(filepath))
+    THROW_EXCEPTION("File " << filepath << " is not a regular file");
+
   std::ifstream ifs(filepath.c_str());
   const std::string contents((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
   return contents;
